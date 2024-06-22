20e155e0c4c4bce01a3c8f9b6e6ae7dc71e1f2a3
Maicon <105327553+maiconlara@users.noreply.github.com>
2024-06-22 19:14:51 -0300

    Update api.ts

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,7 @@
 import useCookie from "@/utils/hooks/useCookies";
 import axios from "axios";
 
-const apiUrl = "https://apiagrocontrol.ddns.net/";
+const apiUrl = "http://143.198.152.106/";
 
 const { getCookie } = useCookie();
 
